0530f0ce9342d6893547b51f760239374618c6c1
OrlandoRocks <odelapena@apexsystems.com>
2024-05-17 18:44:11 -0600

    remove env config

--- src/main.js
+++ src/main.js
@@ -2,29 +2,24 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from "vue-router-prefetch";
 import DashboardPlugin from "./plugins/dashboard-plugin";
-import runtimeEnv from '@mars/heroku-js-runtime-env';
 import App from "./App.vue";
 import router from "./router/router";
 import store from "./store";
 import i18n from "./i18n";
 import "./registerServiceWorker";
 
-const env = runtimeEnv();
 Vue.config.productionTip = false;
 
 Vue.use(DashboardPlugin);
 Vue.use(VueRouter);
 Vue.use(RouterPrefetch);
 
-env.then((env) => {
-  Vue.prototype.$env = env;
-  new Vue({
-    el: "#app",
-    render: (h) => h(App),
-    router,
-    i18n,
-    store,
-  });
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+  router,
+  i18n,
+  store,
 });
 
 let localAuthToken = localStorage.getItem("auth_token");
